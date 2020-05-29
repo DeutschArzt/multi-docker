f46429bd433d20a6a09a7c244b077b56e3a46bf4
Matthew Buhr <mjbuhr@Matthews-MacBook-Pro.local>
2020-05-29 02:05:16 -0500

    adding in custom message to UI

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,9 @@ function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
+            I'll do what I want!
+          </p>
+          <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
